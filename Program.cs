ce3d8cf8b859ed057c8c9b64d2d10f97a512fe79
Abril <abrudomingueznoya@gmail.com>
2025-09-09 13:46:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Edad
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su edad");
+            string linea = Console.ReadLine();
+            int edad = int.Parse(linea);
+
+            if (edad < 18)
+            {
+                Console.WriteLine("Usted todavía no puede votar");
+            }
+            else
+            {
+                if (edad >= 70)
+                {
+                    Console.WriteLine("Usted ya no tiene obligación de votar");
+                }
+                else
+                {
+                    Console.WriteLine("Usted tiene la obligación de votar");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
